9b45803d4452f848733d18aa14bb06f8d18726fb
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2024-04-07 00:01:54 +0800

    Update Circle.java

--- Midterms/GroupProject2/Circle.java
+++ Midterms/GroupProject2/Circle.java
@@ -1,6 +1,7 @@
+package mexer2.prog2.edu.slu;
 
 public class Circle extends Shape {
-    private double radius;
+    private final double radius;
     public Circle(String name, double radius) {
         super(name);
         this.radius = radius;
@@ -8,11 +9,15 @@ public class Circle extends Shape {
 
     @Override
     public double area() {
-        return Math.PI * Math.pow(radius,2);
+        double area = Math.PI * Math.pow(radius,2);
+        String areaString = String.format("%.5f", area);
+        return Double.parseDouble(areaString);
     }
 
     @Override
     public double perimeter() {
-        return Math.PI * (2 * radius);
+        double perimeter = Math.PI * (2 * radius);
+        String perimeterString = String.format("%.5f", perimeter);
+        return Double.parseDouble(perimeterString);
     }
 }
